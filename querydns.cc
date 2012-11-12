b694c9932cf73190be1959cb977af0de139d9931
wchang <e4523744@gmail.com>
2012-11-12 13:17:58 +0000

    add querydns.cc and querydns.h

---
+++ querydns.cc
@@ -0,0 +1,40 @@
+#include "querydns.h"
+
+using namespace std; // NOLINT
+
+bool QueryDns(const char *name,
+              int ecord_type,
+              const char *dns_server,
+              const uint16_t port,
+              string *result)
+{
+  int len;
+  int ans_len = 1024;
+  u_char answer[ans_len];
+  ns_msg ns;
+  ns_rr rr;
+  string string_tmp;
+
+  res_init();
+  _res.nsaddr_list[0].sin_addr.s_addr = inet_addr(dns_server);
+  _res.nsaddr_list[0].sin_port = port;
+  len = res_query(name, C_IN, ecord_type, answer, ans_len);
+  if (len < 0)
+  {
+    herror("res_query");
+    return -1;
+  }
+  len = ns_initparse(answer, len, &ns);
+  ns_parserr(&ns, ns_s_an, len, &rr);
+  string1 = string_tmp.assign(inet_ntoa(*((struct in_addr *)rr.rdata)));
+  *result = string_tmp;
+  return 0;
+}
+
+int main(int argc,char* argv[])
+{
+  string my_string;
+  QueryDns("www.cern.ch", T_A, "137.138.16.5", 53, &my_string);
+  cout << "The IP is :"<< my_string << endl;
+  return 0;
+}
---
+++ querydns.h
@@ -0,0 +1,11 @@
+#ifndef QUERYDNS_H_
+#define QUERYDNS_H_
+
+#include <netinet/in.h>
+#include <arpa/nameser.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <iostream>
+#endif
